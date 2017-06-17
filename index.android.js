12c0405e422e3dc76a0094e15a42d8069cd61db8
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2017-06-17 19:53:06 +0100

    Managed to get it to work on Expo 🚀 But not complely 😰

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
-import { AppRegistry } from 'react-native';
+import Expo from 'expo';
 
 import App from './src/App';
 
-AppRegistry.registerComponent('GlTesting', () => App);
+Expo.registerRootComponent(App);
